009239f0358a0cf5dad225fcd16bcfb5d912846f
OhKanghoon <ggaa96@naver.com>
2021-02-11 23:10:44 +0900

    upgrade Nimble version

--- Package.swift
+++ Package.swift
@@ -5,13 +5,13 @@ import PackageDescription
 let package = Package(
   name: "HLSCachingReverseProxyServer",
   platforms: [
-    .macOS(.v10_11), .iOS(.v8), .tvOS(.v9)
+    .macOS(.v10_11), .iOS(.v9), .tvOS(.v9)
   ],
   products: [
     .library(name: "HLSCachingReverseProxyServer", targets: ["HLSCachingReverseProxyServer"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/Quick/Nimble.git", .revision("50c3f82c3d23af1d6e710226015b3e94b5265f71")),
+    .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.0.0")),
     .package(url: "https://github.com/devxoul/SafeCollection.git", .upToNextMajor(from: "3.1.0")),
   ],
   targets: [
